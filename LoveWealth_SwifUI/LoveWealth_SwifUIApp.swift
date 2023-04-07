debb0f5925942c99122f5935b3def0b6bfe96c71
Akinbande Gbenga <olugbengalium@gmail.com>
2023-04-07 21:41:52 +0100

    Initial Commit

---
+++ LoveWealth_SwifUI/LoveWealth_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveWealth_SwifUIApp.swift
+//  LoveWealth_SwifUI
+//
+//  Created by Gbenga Abayomi on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoveWealth_SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
